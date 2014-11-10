1b5d952ece17b6fc0ca36f0f16dbe8f58885479a
SunnyBat <Sunnybat@yahoo.com>
2014-11-10 14:49:27 -0800

    Changed version to 1.7.6

--- src/paxchecker/PAXChecker.java
+++ src/paxchecker/PAXChecker.java
@@ -8,7 +8,7 @@ import paxchecker.GUI.*;
  */
 public class PAXChecker {
 
-  public static final String VERSION = "1.7.5.5";
+  public static final String VERSION = "1.7.6";
   public static Setup setup;
 
   /**
