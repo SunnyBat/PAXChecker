5798d34cba0f8207ff2d289a696b14f9a2003c36
SunnyBat <Sunnybat@yahoo.com>
2018-11-06 23:02:00 -0800

    Add window titles, prompts for what to do when pressing X button

--- src/main/java/com/github/sunnybat/paxchecker/PAXChecker.java
+++ src/main/java/com/github/sunnybat/paxchecker/PAXChecker.java
@@ -34,7 +34,7 @@ import twitter4j.Twitter;
  */
 public final class PAXChecker {
 
-	public static final String VERSION = "4.0.0 R1";
+	public static final String VERSION = "4.0.0 R2";
 	private static TwitterStreamer myStreamer; // TODO: Factor elsewhere?
 	private static LinkManager myLinkManager; // TODO: Factor elsewhere?
 
